3a0448bad8c5e4e59828e978ee81ffc8d1684cd9
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2017-11-10 00:18:21 -0700

    added edit user query

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -83,13 +83,27 @@ const mutation = new GraphQLObjectType ({
 		deleteUser: {
 			type: UserType,
 			args: {
-				id: { type: GraphQLString },
+				id: { type: new GraphQLNonNull(GraphQLString) },
 			},
 			resolve(parentValue, { id }) {
 				return axios.delete(`http://localhost:3000/users/${id}`)
 					.then(resp => resp.data);
 			}
+		},
+		editUser: {
+			type: UserType,
+			args: {
+				id: { type: new GraphQLNonNull(GraphQLString)},
+				firstName: { type: GraphQLString },
+				age: { type: GraphQLInt },
+				companyId: { type: GraphQLString }
+			},
+			resolve(parentValue, { id, firstName, age, companyId}) {
+				return axios.patch(`http://localhost:3000/users/${id}`, { firstName, age, companyId} )
+					.then(resp => resp.data);
+			}
 		}
+
 	}
 })
 //returns new instance of graphql schema
